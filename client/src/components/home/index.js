23d3a7818939f518f47562fd708880192364570f
Gauri <2016.gauri.sagane@ves.ac.in>
2023-07-23 15:30:49 -0500

    Add code to get products by date and items sold

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import Featured from './featured';
 import SlimPromotion from '../../utils/promotions/slim.block';
-// import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { productsBySort } from '../../store/actions/products.actions';
 
 const slimPromotion = {
     img: '/images/featured/featured_home_3.jpg',
@@ -12,12 +13,20 @@ const slimPromotion = {
 };
 
 const Home = () => {
+    const { bySold, byDate } = useSelector(state => state.products)
+    const dispatch = useDispatch();
 
-    // const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(productsBySort({
+            limit:4, sortBy:'itemSold', order:'desc', where:'bySold'
+        }));
+        dispatch(productsBySort({
+            limit:4, sortBy:'date', order:'desc', where:'byDate'
+        }));
+    },[dispatch])
 
-    // useEffect(() => {
-    //     dispatch()
-    // },[dispatch])
+    console.log(bySold);
+    console.log(byDate);
 
     return(
         <div>
